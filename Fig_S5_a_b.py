58060b27fc255dc7f9805c3b6fac59ff5590b570
awallucks <a.wallucks@gmx.de>
2020-03-05 19:45:06 +0100

    sim files

---
+++ Fig_S5_a_b.py
@@ -0,0 +1,132 @@
+#Calculate interference visibility of |0>+|1> states with weak coherent states
+
+import matplotlib.pyplot as plt
+import numpy as np
+from qutip import *
+
+def mixed_sup(nth = 0.15, scattering = 0.002, wcs1_amp = 0., wcs2_amp = 0.):
+	#Calculate with all the heating present in the mechanical state from the beginning
+
+
+	N = 10                    # Number of mech states
+
+	## 2-Mode operators for optics and mechanics
+	o_op = tensor(destroy(N), qeye(N))
+	m_op = tensor(qeye(N), destroy(N))
+	num_m = m_op.dag() * m_op
+	num_o = o_op.dag() * o_op
+
+	#first simulate the scattering probability
+	#set up initial state
+	opt_mode = coherent_dm(N, 0.)
+	mech_mode = thermal_dm(N, nth)
+	rho_0 = tensor(opt_mode, mech_mode)
+
+	H_om = Qobj.expm( 1j*np.sqrt(scattering) * (m_op.dag()*o_op.dag() + m_op*o_op) )
+	rho_1 = H_om.dag() * rho_0 * H_om
+
+	#sanity checks
+	scattering_check = expect(num_o, rho_1)
+	print('\n\nscattering prob ' + str(scattering_check)[0:6])
+
+	if wcs1_amp == 0:
+		wcs1_amp = np.sqrt(scattering_check)
+	print('Calculating with n_wcs1 = ' + str(wcs1_amp**2)[0:6])
+	wcs1_mode = coherent_dm(N, wcs1_amp)
+
+	#enlarge Hilbert space, call vectors 'optics_a, optics_b, mechanics'
+	rho_2 = tensor(wcs1_mode, rho_1)
+	#operators
+	oa_op = tensor(destroy(N), qeye(N), qeye(N))
+	ob_op = tensor(qeye(N), destroy(N), qeye(N))
+
+	#sanity checks
+	nopt11 = expect(oa_op.dag()*oa_op, rho_2)
+	nopt12 = expect(ob_op.dag()*ob_op, rho_2)
+	print('Blue before BS na = ' + str(nopt11) + ' nb = ' + str(nopt12)[0:6] + ' sum = ' + str(nopt11+nopt12)[0:6])
+
+	#BS
+	theta = np.pi / 2	#BS transmission
+	BS_A = Qobj.expm( 1j/2*theta * (oa_op.dag()*ob_op + oa_op*ob_op.dag()) )
+	rho_3 = BS_A.dag() * rho_2 * BS_A
+
+	#sanity checks
+	nopt21 = expect(oa_op.dag()*oa_op, rho_3)
+	nopt22 = expect(ob_op.dag()*ob_op, rho_3)
+	print('Blue after BS na = ' + str(np.real(nopt21))[0:6] + ' nb = ' + str(np.real(nopt22))[0:6] + ' sum = ' + str(np.real(nopt21+nopt22))[0:6])
+
+
+	#project states onto n=1 and 2 fock basis of optics (heralding)
+	proj = tensor((basis(N, 1) * basis(N, 1).dag() + basis(N, 2) * basis(N, 2).dag() + basis(N, 3) * basis(N, 3).dag()), qeye(N), qeye(N))
+
+	rho_4 = proj.dag() * rho_3 * proj
+	rho_4 = rho_4.unit()
+
+
+	#trace out optics, leave only mechanics
+	rho_4 = rho_4.ptrace(2).unit()
+
+	#sanity
+	n_mech = expect(num(N), rho_4)
+	print('Mechanical excitation n = ' + str(n_mech)[0:6])
+
+	#now the second interference step
+	#maximum visibility is attained for angle pi/2
+	angle = np.pi/2
+
+	if wcs2_amp == 0.:
+		wcs2_amp = np.sqrt(n_mech)
+
+	wcs2_mode = ket2dm(coherent(N, wcs2_amp*np.exp(-1j*angle)))
+
+	#Beamsplitter input modes
+	rho_5 = tensor(wcs2_mode, rho_4)
+
+	#sanity checks
+	nopt11 = expect(tensor(num(N), qeye(N)), rho_5)
+	nopt12 = expect(tensor(qeye(N), num(N)), rho_5)
+	print('Red before BS2 na = ' + str(nopt11)[0:6] + ' nb = ' + str(nopt12)[0:6] + ' sum = ' + str(nopt11+nopt12)[0:6])
+
+	#Beamsplitter transmission
+	T = 0.5
+
+	#Calculate mode in one output arm of beamsplitter
+	n_out1 = np.real(T*expect(num_m, rho_5) + (1-T)*expect(num_o, rho_5) + 1j* np.sqrt(T)*np.sqrt(1-T) * expect(o_op.dag()*m_op-o_op*m_op.dag(), rho_5))
+
+	n_out2 = np.real((1-T)*expect(num_m, rho_5) + T*expect(num_o, rho_5) - 1j* np.sqrt(T)*np.sqrt(1-T) * expect(o_op.dag()*m_op-o_op*m_op.dag(), rho_5))
+
+	print('Red after BS2 na = ' + str(n_out1)[0:6] + ' nb = ' + str(n_out2)[0:6] + ' sum = ' + str(n_out1+n_out2)[0:6])
+
+	vis = (n_out1-n_out2) / (n_out1+n_out2)
+	print('Max vis V = ' + str(100 * vis)[0:6] + '%')
+	return [np.real(n_mech), np.real(vis)]
+
+
+
+pts1 = 31
+pts2 = 4
+p_blue = 0.002
+wcs1_amp = np.linspace(0.0001, 0.02, pts1)
+ntherm = [0.00, 0.05, 0.15, 0.30]
+v = np.zeros([pts2, pts1])
+n = np.zeros([pts2, pts1])
+
+ax1 = plt.subplot(2, 1, 1)
+ax2 = plt.subplot(2, 1, 2)
+for jj in range(pts2):
+	for ii in range(pts1):
+		[n[jj, ii], v[jj, ii]] = mixed_sup(ntherm[jj], p_blue, np.sqrt(wcs1_amp[ii]), 0.)
+
+	ax1.plot(wcs1_amp, v[jj, :])
+	ax2.plot(wcs1_amp, np.sqrt(n[jj, :]))
+
+np.savetxt('blue_n_fock.dat', n)
+np.savetxt('blue_vis.dat', v)
+
+ax2.set_xlabel('n_coherent')
+ax1.set_ylabel('vis')
+ax2.set_ylabel('n_mech')
+plt.legend(['n_th = 0.00', '0.05', '0.15', '0.30' ])
+plt.show()
+
+print('done')
